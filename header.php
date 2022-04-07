542136a43833c56582b6b737aaaafd9e7b9dc56e
210User <ispr24-39_beluakovds@pgk63.ru>
2022-04-07 17:49:35 +0400

    PHP

---
+++ header.php
@@ -0,0 +1,14 @@
+<?php
+	/**
+	* Contains the header.
+	*/
+?><!doctype html>
+<html <?php language_attributes();?>>
+	<head>
+		<meta charset="<?php bloginfo('charset') ?>">
+		<meta name="viewport" content="width=device-width, intial-scale=1">
+	</head>
+		
+		<?php wp_head(); ?>
+	<body <?php body_class() ?>>
+	 <h1>dkalsd</h1>
